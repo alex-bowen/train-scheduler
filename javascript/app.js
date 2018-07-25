0460f59116abd6d162cc574f87e1db3d9dbbe75b
alex-bowen <alexbowen2017@gmail.com>
2018-07-25 17:42:00 -0500

    Styled and ready for functionality

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAwsN2Wrp9wGYkwkwzWYlht2RpJRRoXMyw",
+    authDomain: "train-scheduler-alex-bowen.firebaseapp.com",
+    databaseURL: "https://train-scheduler-alex-bowen.firebaseio.com",
+    projectId: "train-scheduler-alex-bowen",
+    storageBucket: "train-scheduler-alex-bowen.appspot.com",
+    messagingSenderId: "782417296585"
+  };
+  
+  firebase.initializeApp(config);
